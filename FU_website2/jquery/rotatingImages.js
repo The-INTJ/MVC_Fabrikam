159a4ba588158df9390ee111705bff07b06585b5
Drew Taylor <drew.taylor@perficient.com>
2020-06-03 13:44:52 -0400

    beginning changes

--- FU_website2/jquery/rotatingImages.js
+++ FU_website2/jquery/rotatingImages.js
@@ -1,27 +1,38 @@
 /* Handles image rotation behavior */
 
 // Global vars
-var imgLoc, rotating = 0;
+var imgLoc = 0;
 var imgs = ["images/Fab_logo.png", "images/FU_image.png", "images/picture.png", "images/Fab_logo.png"]
 var imgSrc;
 
 // Set the images to rotate
-rotating = true;
-setInterval(rotate, 4000); // every 4 seconds
+var rotating = true;
+setInterval(rotate, 1000); // every 4 seconds
 
 // Select image on radio button functionality
-$("div.imgrot > input").on("click", function () {
+$("div.imgrot >> input").on("click", function () {
     stopRotating();
     imgLoc = $(this).val(); // Radio button number
     setImage(imgLoc);
     setTimeout(keepRotating, 3000); // Resumes rotation after 3 seconds
 });
 
+// Animation functionality
+/*
+$("div.imgrot > img").animate({
+    left: 300
+});
+*/
+
+function slide(img1, img2) {
+
+}
 
 // Helper functions
 function rotate() {
     if (rotating) {
         setImage(imgLoc);
+        setRadioButton(imgLoc);
         imgLoc++;
         if (imgLoc > 3) {
             imgLoc = 0;
@@ -31,7 +42,7 @@ function rotate() {
 
 function setImage(loc) {
     imgSrc = imgs[loc]; // Set path
-    $("img#changable").attr("src", imgSrc); // set source to path
+    $("img#current").attr("src", imgSrc); // set source to path
 }
 
 function keepRotating() {
@@ -40,4 +51,8 @@ function keepRotating() {
 
 function stopRotating() {
     rotating = false;
+}
+
+function setRadioButton(loc) {
+    $('input:radio[name=place]')[loc].checked = true;
 }
