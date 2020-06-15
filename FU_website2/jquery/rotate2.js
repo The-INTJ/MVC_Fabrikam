f3e4a7e31a0aa6b1dcbf621d1cc888841e71ebf9
Drew Taylor <drew.taylor@perficient.com>
2020-06-15 16:00:33 -0400

    Attempting a much cleaner code of image rotate

--- FU_website2/jquery/rotate2.js
+++ FU_website2/jquery/rotate2.js
@@ -4,6 +4,7 @@ function rotate(num) {
 
     var imgLoc = 0;
     var tracker = 0;
+    var go = true;
     setRadioButton(imgLoc);
 
     var slider = $("#jquery-slideshow"), // cache the slider object for later use
@@ -22,13 +23,13 @@ function rotate(num) {
                     while (prev < curr) {
                         slider.children().next().prependTo(slider);
                         prev++;
+                        
                     }
                     //slider.children("li:first").prependTo(slider);
                     slider.css("left", 0);
             });
         }
 
-        //setInterval(forward, 1000);
 
         const backward = (prev, curr) => {
             tracker = (tracker - 1) % 4;
@@ -44,7 +45,17 @@ function rotate(num) {
             }, 3000, "swing");
         }
 
+        setInterval(function () {
+            var currLoc = $("div.imgrot >> input").val();
+            var nextImg = (currLoc + 1) % 4;
+            if (go) {
+                forward(currLoc, nextImg);
+            }
+        }, 1000);
+
         $("div.imgrot >> input").on("click", function () {
+
+            setInterval(function () { go = true; }, 3000);
             go = false;
             var prevLoc = imgLoc;
             imgLoc = $(this).val(); // Radio button number
