1852659226dcfcaf0792b768d6146b3ce7100926
Drew Taylor <drew.taylor@perficient.com>
2020-06-19 15:51:13 -0400

    Downloaded React, attempting dynamic loading of blog page

--- MVC-fab/MVC_Version/MVC_Version/Scripts/custom/GenerateBlogs.js
+++
@@ -1,4 +0,0 @@
-var body = document.getElementsByTagName('body')[0],
-    newdiv = document.createElement('div');   //create a div
-newdiv.id = 'newid';                      //add an id
-body.appendChild(newdiv);
