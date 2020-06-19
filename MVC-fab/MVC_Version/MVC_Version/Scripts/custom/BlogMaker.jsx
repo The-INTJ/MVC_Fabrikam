bab9495c85b05328322be7a0995b843c232ede67
Drew Taylor <drew.taylor@perficient.com>
2020-06-19 16:56:00 -0400

    Debugging React integration issues

---
+++ MVC-fab/MVC_Version/MVC_Version/Scripts/custom/BlogMaker.jsx
@@ -0,0 +1,21 @@
+﻿//var dropdown =
+//    <Dropdown>
+//        A dropdown list
+//        <Menu>
+//            <MenuItem>Do Something</MenuItem>
+//            <MenuItem>Do Something Fun!</MenuItem>
+//            <MenuItem>Do Something Else</MenuItem>
+//        </Menu>
+//    </Dropdown>;
+
+//render(dropdown);
+
+class HelloWorld extends React.Component {
+    render() {
+        return (
+            < div >
+< h1 > Hello World React JS! < /h1>
+< /div>)
+}
+}
+ReactDOM.render( < HelloWorld /> , document.getElementById('myContainer')); 
